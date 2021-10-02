f7d1dfad0830a79e043a4c6cc3b62213ea03b98c
ryanrosello-og <50574915+ryanrosello-og@users.noreply.github.com>
2021-10-02 18:23:39 +1000

    multiple viewers

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('playwright-vscode-trace-viewer.helloWorld', (...args) => {
+	let disposable = vscode.commands.registerCommand('playwright-vscode-trace-viewer.helloWorld', async (...args) => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('Hello World from playwright-vscode-trace-viewer!');
@@ -22,46 +22,29 @@ export function activate(context: vscode.ExtensionContext) {
 		if (args && args.length > 0) {
 			path = args[0].fsPath;
 			// if zip file
+			let t = await vscode.tasks.fetchTasks();
+
+			// vscode.tasks.taskExecutions[0].task.execution
+
+
 			const shellExec = new vscode.ShellExecution(`npx playwright show-trace ${path} `);
-
+			let tsk = new vscode.Task({ type: `typesinstaller${Math.random()}` },
+				vscode.TaskScope.Workspace,
+				`typesinstaller${Math.random()}`,
+				`typesinstaller${Math.random()}`,
+				shellExec,
+				'npm')
+			tsk.isBackground = true
+			tsk.runOptions.reevaluateOnRerun = true
 			try {
-
-				vscode.tasks.executeTask(
-					new vscode.Task({ type: 'typesinstaller' },
-						vscode.TaskScope.Workspace,
-						'TypesInstaller',
-						'Types Installer',
-						shellExec,
-						'npm'));
+				vscode.tasks.executeTask(tsk
+				);
 			} catch (error) {
 				console.log(error)
 			}
 		} else {
 			return;
 		}
-
-
-
-
-		// const editor = vscode.window.activeTextEditor;
-		// if (editor) {
-		// 	console.log('has context')
-		// 	const p = editor.document.uri.path;
-		// 	console.log(p)
-		// 	// if (!editor.document.fileName.endsWith('zip')) {
-		// 	// 	return;
-		// 	// }
-		// 	const shellExec = new vscode.ShellExecution(`npx playwright show-trace C:\\ryanrosello-og\\vscode-ext\\pw-tyscript\\test-results\\tests-foo-dev-Page\\trace.zip `);
-
-		// 	vscode.tasks.executeTask(
-		// 		new vscode.Task({ type: 'typesinstaller' },
-		// 			vscode.TaskScope.Workspace,
-		// 			'TypesInstaller',
-		// 			'Types Installer',
-		// 			shellExec,
-		// 			'npm'));
-		// }
-		// vscode.window.setStatusBarMessage(`files.length > ${args.length}`, 3000);
 	});
 
 	context.subscriptions.push(disposable);
